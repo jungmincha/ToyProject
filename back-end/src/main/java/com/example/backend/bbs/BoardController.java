2271159722c71a41787f964f35d4a4200a6b9d57
jungmincha <prttyjimmy@naver.com>
2021-08-05 23:52:00 +0900

    test commit

--- back-end/src/main/java/com/example/backend/bbs/BoardController.java
+++ back-end/src/main/java/com/example/backend/bbs/BoardController.java
@@ -21,7 +21,7 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-public class BoardController {//test2
+public class BoardController {//test3
 
     private final BoardService boardService;
 
